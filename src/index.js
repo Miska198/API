6246f9d18c8a9761897077c96a929fa4390fb0a5
Miska198 <43399759+Miska198@users.noreply.github.com>
2019-01-16 09:21:45 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+import Layout from "./components/API";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<API />, rootElement);
